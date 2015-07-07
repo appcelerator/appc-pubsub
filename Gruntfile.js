c98e231826784db016fd9b7bb19239da13c656d1
Jeff Haynie <jhaynie@appcelerator.com>
2015-07-07 09:24:49 -0700

    added Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function (grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		mochaTest: {
+			options: {
+				timeout: 30000,
+				reporter: 'spec',
+				ignoreLeaks: false
+			},
+			src: ['test/**/*.js']
+		},
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			src: ['*.js','lib/**/*.js','test/**/*.js']
+		},
+		kahvesi: {
+			src: ['test/**/*.js']
+		},
+		clean: ['tmp']
+	});
+
+	// Load grunt plugins for modules
+	grunt.loadNpmTasks('grunt-mocha-test');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-kahvesi');
+
+	// register tasks
+	grunt.registerTask('cover', ['kahvesi', 'clean']);
+	grunt.registerTask('default', ['jshint', 'mochaTest', 'clean']);
+};
